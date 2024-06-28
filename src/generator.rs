f37541c1c879966a848aa5a00e242f04fd8eab27
Ivan Mikushin <imikushin@users.noreply.github.com>
2024-06-28 15:32:19 -0700

    Produce matrices U and W

--- src/generator.rs
+++ src/generator.rs
@@ -1,4 +1,4 @@
-use ark_ff::PrimeField;
+use ark_ff::{Field, PrimeField};
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
 use ark_relations::r1cs::{
     ConstraintMatrices, ConstraintSynthesizer, ConstraintSystem, Matrix, OptimizationGoal,
@@ -38,25 +38,16 @@ where
 
         ///////////////////////////////////////////////////////////////////////////
 
-        // TODO transform R1CS matrices A, B, C into SAP U and W
-
-        let m0 = cs.num_instance_variables();
-
-        let matrices = cs.to_matrices().unwrap();
-
-        let (u, w): (Matrix<F>, Matrix<F>) = Self::transform_matrices(matrices, m0);
-        // let (i, j) = (0, 10);
-        // let u_ij = u[i]
-        //     .iter()
-        //     .find(|(v, index)| *index == j)
-        //     .unwrap_or(&(F::zero(), 0))
-        //     .0;
+        let sap_matrices = SAPMatrices {
+            r1cs_matrices: cs.to_matrices().unwrap(),
+        };
 
         ///////////////////////////////////////////////////////////////////////////
 
         let domain_time = start_timer!(|| "Constructing evaluation domain");
 
-        let num_constraints = u.len();
+        let (n, m) = sap_matrices.size();
+        let num_constraints = n;
         let domain = D::new(num_constraints).ok_or(SynthesisError::PolynomialDegreeTooLarge)?;
 
         end_timer!(domain_time);
@@ -77,4 +68,116 @@ where
     fn transform_matrices(matrices: ConstraintMatrices<F>, m0: usize) -> (Matrix<F>, Matrix<F>) {
         todo!()
     }
+}
+
+struct SAPMatrices<F: Field> {
+    r1cs_matrices: ConstraintMatrices<F>,
+}
+
+impl<F: Field> SAPMatrices<F> {
+    fn size(&self) -> (usize, usize) {
+        let (m0, m, n) = self.m0_m_n();
+
+        ((m0 + n) * 2, m0 * 2 + m + n)
+    }
+
+    fn u(&self, i: usize, j: usize) -> F {
+        let matrices = &self.r1cs_matrices;
+
+        let (m0, m, n) = self.m0_m_n();
+        let (double_m0, double_m0_plus_n, double_m0_plus_double_n, m0_plus_m) =
+            Self::inner_size_bounds(m0, m, n);
+
+        let zero = F::zero();
+        let one = F::one();
+        let minus_one = -one;
+        let two = one + one;
+
+        match (i, j) {
+            (0, 0) => two,                                       // (A₀+1)₀₀=2
+            (i, 0) if i < m0 => one,                             // (A₀+1)ᵢ₀=1
+            (i, j) if i < m0 && j == i => one,                   // (A₀+1)ᵢⱼ=1
+            (i, 0) if i == m0 => zero,                           // (A₀-1)₀₀=0
+            (i, 0) if i < double_m0 => one,                      // (A₀-1)ᵢ₀=1
+            (i, j) if i < double_m0 && j == i - m0 => minus_one, // (A₀-1)ᵢⱼ=-1
+
+            (i, _) if i < double_m0 => zero,
+            (_, j) if j < m0 => zero,
+
+            (i, j) if i < double_m0_plus_n && j < m0_plus_m => {
+                let (i, j) = (i - double_m0, j - m0);
+                m_at(&matrices.a, i, j) + m_at(&matrices.b, i, j)
+            }
+            (i, j) if i < double_m0_plus_double_n && j < m0_plus_m => {
+                let (i, j) = (i - double_m0_plus_n, j - m0);
+                m_at(&matrices.a, i, j) - m_at(&matrices.b, i, j)
+            }
+            (_, _) => zero,
+        }
+    }
+
+    fn w(&self, i: usize, j: usize) -> F {
+        let matrices = &self.r1cs_matrices;
+
+        let (m0, m, n) = self.m0_m_n();
+        let (double_m0, double_m0_plus_n, double_m0_plus_double_n, m0_plus_m) =
+            Self::inner_size_bounds(m0, m, n);
+
+        let zero = F::zero();
+        let one = F::one();
+        let two = one + one;
+        let four = two + two;
+
+        match (i, j) {
+            (i, j) if i < m0 && j == i + m0 => four,
+            (i, j) if i < m0 && j == i + m0_plus_m => one,
+            (i, _) if i < m0 => zero,
+
+            (i, j) if i < double_m0 && j == i + m => one,
+
+            (i, _) if i < double_m0 => zero,
+            (_, j) if j < m0 => zero,
+
+            (i, j) if i < double_m0_plus_n && j < m0_plus_m => {
+                let (i, j) = (i - double_m0, j - m0);
+                m_at(&matrices.c, i, j) * four
+            }
+
+            (i, j) if i < double_m0_plus_n && j == i + m => one,
+            (i, j) if i < double_m0_plus_n => zero,
+
+            (i, j) if i < double_m0_plus_double_n && j == i - n + m => one,
+
+            (_, _) => zero,
+        }
+    }
+
+    fn inner_size_bounds(m0: usize, m: usize, n: usize) -> (usize, usize, usize, usize) {
+        let double_m0 = m0 + m0;
+        let double_m0_plus_n = double_m0 + n;
+        let double_m0_plus_double_n = double_m0_plus_n + n;
+        let m0_plus_m = m0 + m;
+        (
+            double_m0,
+            double_m0_plus_n,
+            double_m0_plus_double_n,
+            m0_plus_m,
+        )
+    }
+
+    fn m0_m_n(&self) -> (usize, usize, usize) {
+        let matrices = &self.r1cs_matrices;
+
+        let m0 = matrices.num_instance_variables;
+        let m = m0 + matrices.num_witness_variables; // full witness size (public + private)
+        let n = matrices.num_constraints;
+        (m0, m, n)
+    }
+}
+
+fn m_at<F: Field>(m: &Matrix<F>, i: usize, j: usize) -> F {
+    m[i].iter()
+        .find(|(v, index)| *index == j)
+        .unwrap_or(&(F::zero(), 0))
+        .0
 }
